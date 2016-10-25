50e7027ddd18114c8d537965797bc24fdf35266f
Michael Novotny <manovotny@gmail.com>
2016-10-25 14:26:33 -0500

    Changes cache format to json.

--- cli.js
+++ cli.js
@@ -8,27 +8,35 @@ const pathExists = require('path-exists');
 const parser = itunes.parser();
 const stream = fse.createReadStream(`${os.homedir()}/Music/iTunes/iTunes Library.xml`);
 
-const current = [];
+const music = [];
 
 parser.on('track', (track) => {
     if (track.hasOwnProperty('Bit Rate')) {
-        current.push(`${track['Artist']} - ${track['Name']} // ${track['Album']}`);
+        music.push(`${track['Artist']} - ${track['Name']} // ${track['Album']}`);
     }
 });
 
 stream.on('close', () => {
-    const cachePath = `${os.homedir()}/.itunes/cache.txt`;
+    const cachePath = `${os.homedir()}/.itunes/cache.json`;
 
-    if (pathExists.sync(cachePath)) {
-        const cache = fse.readFileSync(cachePath, {encoding: 'utf8'}).split('\n');
-        const removed = cache.filter((x) => current.indexOf(x) < 0);
-
-        if (removed.length) {
-            fse.writeFileSync(`${os.homedir()}/.itunes/removed-${moment().format('YYYY-MM-DD@hh.mm.ssa')}.txt`, removed.join('\n'));
-        }
+    if (!pathExists.sync(cachePath)) {
+        fse.writeJSONSync(cachePath, {
+            music: []
+        });
     }
 
-    fse.writeFileSync(cachePath, current.join('\n'));
+    const cache = fse.readJSONSync(cachePath);
+    const removed = cache.music.filter((x) => music.indexOf(x) < 0).filter(String);
+
+    if (removed.length) {
+        fse.writeFileSync(`${os.homedir()}/.itunes/removed-${moment().format('YYYY-MM-DD@hh.mm.ssa')}.txt`, {
+            music: removed
+        });
+    }
+
+    fse.writeJSONSync(cachePath, {
+        music: music
+    });
 });
 
 stream.pipe(parser);
