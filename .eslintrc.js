716731e0276da656e8f024f21cfe141d4246b61a
Michael Novotny <manovotny@gmail.com>
2016-10-25 15:54:23 -0500

    Updates eslint-config-get-off-my-lawn.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
     extends: 'get-off-my-lawn',
     rules: {
-        'no-sync': 0,
-        // can be removed when new release of get-off-my-lawn is made.
-        'arrow-body-style': 0,
-        'no-magic-numbers': 0,
-        'unicorn/explicit-length-check': 0
+        'no-sync': 0
     }
 };
